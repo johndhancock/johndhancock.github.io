dfe8ddcfa9aad8ebad95c7a521361a3de2c132a9
jdhancock88 <jdhancock@gmail.com>
2019-01-24 17:14:31 -0600

    Gets gulp babelify/browserify working

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,12 @@
-
+import app from './modules/app.json';
 document.addEventListener('DOMContentLoaded', function (event) {
 
-  const testData = [{'head': 'my headline'}];
+  const testData = [{'head': 'headline'}];
   console.log('ready to es6!');
   const foo = 4;
-  console.log(foo);
-  console.log(testData);
+  const bar = 5;
+  console.log(foo, bar);
+  console.log(app[0].head);
 });
 //
 // const app = new App();
