68b24c23ac178a17d196be260dc6583fb52a58b2
jdhancock88 <jdhancock@gmail.com>
2019-03-06 22:51:09 -0600

    Finishes site.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,11 +5,21 @@ import projects from './modules/design.json';
 document.addEventListener('DOMContentLoaded', (e) => {
   const source = document.getElementById('work-template').innerHTML;
   const workTemplate = Handlebars.compile(source);
-  const workGrid = document.getElementById('work__grid');
 
   projects.forEach((project) => {
     const projectBlock = workTemplate(project);
     $('#work__grid').append(projectBlock);
+  });
+
+  const date = new Date();
+  const year = date.getFullYear();
+  $('#footer-year').text(year);
+
+  $('nav a').click(function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top,
+    }, 500);
   });
 });
 //
